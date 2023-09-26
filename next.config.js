0d95e9ab527b0ec2d0d082d9c40ee4c99e3fea70
leeharyung <lhr4884@naver.com>
2023-09-26 17:47:45 +0900

    fix: next config 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 
+const config = {
+  output: 'export'
+}
+  
 const nextConfig = withPWA({
   dest: 'public',
-})
+})(config)
 
 module.exports = nextConfig
