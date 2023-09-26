0cc8cafcc9583443f70f8f8db0fdd8b770824663
leeharyung <lhr4884@naver.com>
2023-09-26 17:30:30 +0900

    feat: nextConfig에 pwa 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-}
+const withPWA = require('next-pwa')
+
+const nextConfig = withPWA({
+  dest: 'public',
+})
 
 module.exports = nextConfig
