6e5569c59ff0effd769a187a29ddf2ac084f903c
leeharyung <lhr4884@naver.com>
2023-09-28 01:12:22 +0900

    feat: logo 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="text-h1 text-primary-main bg-primary-50">hi 안녕하세요</div>
+    <div className="text-h1 text-primary-main bg-primary-50">
+      hi 안녕하세요
+      <Image
+        src="/assets/icons/logo.svg"
+        alt="핑퐁 로고"
+        width="280"
+        height="170"
+      />
+    </div>
   );
 }
