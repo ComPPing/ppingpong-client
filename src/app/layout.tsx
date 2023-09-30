abaab4c4db3e64d7700744fb412055b837ea6dac
leeharyung <lhr4884@naver.com>
2023-10-01 02:46:21 +0900

    feat: add metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,22 @@ const notoSansKR = Noto_Sans_KR({
 export const metadata: Metadata = {
   title: 'ppingpong',
   description: '위치기반 장소 추천 서비스',
+  openGraph: {
+    title: 'ppingpong',
+    description: '위치기반 장소 추천 서비스',
+    type: 'website',
+    siteName: 'ppingpong',
+    locale: 'ko_KR',
+    url: 'https://ppingpong.pages.dev/',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: '핑퐁 로고',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
