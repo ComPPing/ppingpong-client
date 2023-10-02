0d4eeab3642e59d8e1a92780d1e2ef231994ba75
leeharyung <lhr4884@naver.com>
2023-10-02 20:02:03 +0900

    feat: 로그인 ui 제작

---
+++ src/app/login/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+import { OAuthButton } from '@/components/button';
+
+const page = () => {
+  return (
+    <main className="h-screen bg-primary-50 fixed w-screen flex items-center justify-center flex-col p-6">
+      <div className="flex-1 flex items-center">
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="핑퐁 로고"
+          width="280"
+          height="170"
+        />
+      </div>
+      <div className="flex flex-col b-0 gap-2 w-full">
+        <OAuthButton provider="kakao" />
+        <OAuthButton provider="google" />
+      </div>
+    </main>
+  );
+};
+
+export default page;
