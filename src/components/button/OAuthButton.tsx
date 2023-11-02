71f8b90e70345ef3d86d4f117d6d0bd13c70eec2
leeharyung <lhr4884@naver.com>
2023-11-02 18:39:40 +0900

    fix: oauth 버튼 url 수정

--- src/components/button/OAuthButton.tsx
+++ src/components/button/OAuthButton.tsx
@@ -8,7 +8,7 @@ import { calculateStyle } from '@/utils/calculateStyle';
 import { Button } from './Button';
 
 const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
-const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email&response_type=code`;
+const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20profile%20email%20https://www.googleapis.com/auth/user.birthday.read%20https://www.googleapis.com/auth/user.gender.read`;
 
 interface SocialLoginButtonProps {
   provider: 'kakao' | 'google';
