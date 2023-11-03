f2d096bf4a8c9f5a8d0dd05ce26953434fb3f6f1
leeharyung <lhr4884@naver.com>
2023-11-03 18:47:33 +0900

    feat: 메세지 받을 때 자동 스크롤 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Message } from '@/components/message';
 
@@ -24,9 +24,21 @@ const DUMMY_DATA: MessageType[] = [
 export default function Home() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState<MessageType[]>(DUMMY_DATA);
+  const contentsRef = useRef<HTMLOListElement>(null);
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
+
+  useEffect(() => {
+    localStorage.getItem('token') ?? window.location.replace('/login');
+  }, []);
+
+  useEffect(() => {
+    contentsRef.current?.scrollTo({
+      top: contentsRef.current?.scrollHeight + 100,
+      behavior: 'smooth',
+    });
+  }, [messages]);
 
   const handleKeyUpInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (input === '') return;
@@ -45,35 +57,29 @@ export default function Home() {
     }
   };
 
-  // @fixme: 임시로 로그인 체크를 위해 작성
-  useEffect(() => {
-    localStorage.getItem('token') ?? window.location.replace('/login');
-  }, []);
   return (
-    <div className="flex flex-col h-full">
-      <header className="p-4 border-b-2">
+    <div className="h-screen flex flex-col relative">
+      <header className="p-3 border-b-2">
         <Image
-          src="/assets/icons/logo.svg"
+          src="/assets/images/smallLogo.png"
           alt="logo"
           width="140"
           height="30"
         />
       </header>
-      <main className="flex-1 overflow-scroll">
-        <ol className="text-body2">
-          {messages.map((message) => (
-            <Message
-              text={message.text}
-              sender={message.sender}
-              key={message.id}
-            />
-          ))}
-        </ol>
-      </main>
-      <div className="border-t-2 border-gray-100 w-full px-4 pt-2.5 pb-6">
+      <ol className="text-body2 overflow-scroll flex-1" ref={contentsRef}>
+        {messages.map((message) => (
+          <Message
+            text={message.text}
+            sender={message.sender}
+            key={message.id}
+          />
+        ))}
+      </ol>
+      <div className="border-t-2 border-gray-100 w-full px-4 pt-3 pb-8 sticky bottom-0">
         <input
           type="text"
-          className="border-2 p-1 border-gray-200 bg-gray-50 rounded-md w-full h-8 text-body3 focus:outline-none"
+          className="border-2 p-1 border-gray-200 bg-gray-50 rounded-md w-full h-10 text-body2 focus:outline-none"
           onChange={handleChangeInput}
           onKeyUp={handleKeyUpInput}
           value={input}
