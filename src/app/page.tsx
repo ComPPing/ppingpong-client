06a78ea22c2ae5b651d466fcc9b4b9b183c69109
halang <lhr4884@naver.com>
2023-11-11 19:07:57 +0900

    fix: input 창 UI 변경 (#21)
    * feat: user-select 막기
    
    * fix: input창 UI 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useRef, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
 import { Message } from '@/components/message';
 
 type MessageType = {
-  id: number;
+  id: string;
   sender: 'me' | 'other';
   text: string;
   time: Date;
@@ -14,7 +14,7 @@ type MessageType = {
 
 const DUMMY_DATA: MessageType[] = [
   {
-    id: 0,
+    id: '0',
     sender: 'other',
     text: '반가워 친구야~ 궁금한거 있으면 뭐든 물어봐!',
     time: new Date(),
@@ -26,7 +26,6 @@ export default function Home() {
   const [messages, setMessages] = useState<MessageType[]>(DUMMY_DATA);
   const contentsRef = useRef<HTMLOListElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
-  const homeComponentRef = useRef<HTMLDivElement>(null);
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
@@ -54,7 +53,7 @@ export default function Home() {
     setMessages((prev) => [
       ...prev,
       {
-        id: messages.length + 1,
+        id: (messages.length + 1).toString(),
         sender: 'me',
         text: input,
         time: new Date(),
@@ -65,19 +64,16 @@ export default function Home() {
   };
 
   return (
-    <div className="h-full max-h-screen flex flex-col" ref={homeComponentRef}>
-      <header className="p-3 border-b-2">
+    <Fragment>
+      <header className="p-3 border-b-2 w-full bg-white/95">
         <Image
           src="/assets/images/smallLogo.png"
           alt="logo"
           width="140"
-          height="30"
+          height="28"
         />
       </header>
-      <ol
-        className="text-body2 overflow-scroll h-[calc(100%_-_140px)]"
-        ref={contentsRef}
-      >
+      <ol className="text-body2 overflow-scroll flex-1" ref={contentsRef}>
         {messages.map((message) => (
           <Message
             text={message.text}
@@ -86,18 +82,18 @@ export default function Home() {
           />
         ))}
       </ol>
-      <div className="border-t-2 border-gray-100 w-full px-4 pt-3 pb-5 fixed bottom-0">
-        <div className="border-2 border-gray-200 bg-gray-50 rounded-md flex">
+      <div className="border-t-2 border-gray-100 px-2 pt-2 pb-6 bg-white">
+        <div className="border-2 border-gray-200 rounded-xl flex bg-none">
           <input
             type="text"
-            className="p-1 w-full text-body2 focus:outline-none"
+            className="p-1.5 w-full text-body2 focus:outline-none rounded-full"
             onChange={handleChangeInput}
             onKeyUp={handleKeyUpInput}
             value={input}
             ref={inputRef}
           />
           <button
-            className="flex justify-center items-center"
+            className="flex justify-center items-center rounded-xl bg-gray-100 m-1"
             onClick={handleSendMessage}
           >
             <Image
@@ -105,11 +101,11 @@ export default function Home() {
               alt="send"
               width="24"
               height="24"
-              className="mx-2"
+              className="m-2"
             />
           </button>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
