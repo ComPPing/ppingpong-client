3bce4b2079b822bc34f43d34e87e096f729363ae
leeharyung <lhr4884@naver.com>
2023-11-14 04:19:18 +0900

    feat: menubar 추가

---
+++ src/components/menubar/Menubar.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+export const Menubar = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="fixed bottom-0 w-full flex py-4 px-8 border-t-2 justify-between items-center">
+      <Link href="/main" as="">
+        <Image
+          src="/assets/icons/home.svg"
+          alt="홈 화면 가기"
+          width="25"
+          height="25"
+          className={
+            pathname === '/main' || pathname === '/'
+              ? 'opacity-100'
+              : 'opacity-40'
+          }
+        />
+      </Link>
+      <Link href="/chat">
+        <Image
+          src="/assets/icons/chat.svg"
+          alt="채팅창 가기"
+          width="25"
+          height="25"
+          className={pathname === '/chat' ? 'opacity-100' : 'opacity-40'}
+        />
+      </Link>
+      <Link href="/main/bookmark">
+        <Image
+          src="/assets/icons/bookmark.svg"
+          alt="북마크 페이지 가기"
+          width="25"
+          height="25"
+          className={
+            pathname === '/main/bookmark' ? 'opacity-100' : 'opacity-40'
+          }
+        />
+      </Link>
+      <Link href="/main/user">
+        <Image
+          src="/assets/icons/user.svg"
+          alt="설정페이지 가기"
+          width="25"
+          height="25"
+          className={pathname === '/main/user' ? 'opacity-100' : 'opacity-40'}
+        />
+      </Link>
+    </div>
+  );
+};
