5a00b803add37e93288b53467d50b6d37e80c4a5
leeharyung <lhr4884@naver.com>
2023-11-21 00:41:16 +0900

    feat: deleteCookie 추가

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,4 +1,8 @@
 export function getCookie(name: string) {
   const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-  return value ? value[2] : null;
+  return value ? decodeURIComponent(value[2]) : null;
+}
+
+export function deleteCookie(name: string) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
 }
