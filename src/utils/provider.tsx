859945f008fb74d161d92d5a20baf7c1f15aa1a5
leeharyung <lhr4884@naver.com>
2023-11-20 22:42:51 +0900

    feat: QueryClientProvider 추가

---
+++ src/utils/provider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { PropsWithChildren, useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000 * 7,
+          refetchOnWindowFocus: false,
+        },
+      },
+    }),
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
